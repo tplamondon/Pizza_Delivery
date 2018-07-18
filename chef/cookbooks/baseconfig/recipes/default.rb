65912e04b3a70777a81f3c548f638c77bcc41166
tplamond <tplamond@sfu.ca>
2018-07-18 13:36:02 -0700

    Ruby on Rails base

---
+++ chef/cookbooks/baseconfig/recipes/default.rb
@@ -0,0 +1,56 @@
+# Make sure the Apt package lists are up to date, so we're downloading versions that exist.
+cookbook_file "apt-sources.list" do
+  path "/etc/apt/sources.list"
+end
+execute 'apt_update' do
+  command 'apt-get update'
+end
+
+# Base configuration recipe in Chef.
+package "wget"
+package "ntp"
+
+cookbook_file "ntp.conf" do
+  path "/etc/ntp.conf"
+end
+execute 'ntp_restart' do
+  command 'service ntp restart'
+end
+
+# Rails stuff
+# base configuration for rails
+package 'ruby-dev'
+package 'sqlite3'
+package 'libsqlite3-dev'
+package 'zlib1g-dev'
+package 'nodejs'
+package 'build-essential'
+
+
+execute 'bundler install' do
+  command 'sudo gem install bundler'
+end
+
+execute 'install rails' do
+  command 'sudo gem install rails'
+end
+
+execute 'bundle' do
+  cwd '/home/vagrant/project/pizza_delivery'
+  #user 'vagrant'
+  command 'bundle update actionpack'
+  command 'bundle install'
+end
+
+execute 'migrate' do
+  cwd '/home/vagrant/project/pizza_delivery'
+  #user 'vagrant'
+  command 'rake db:migrate'
+end
+
+
+execute 'host server' do
+  cwd '/home/vagrant/project/pizza_delivery'
+  command './bin/rails server -d -b 0.0.0.0'
+  #user 'vagrant'
+end
