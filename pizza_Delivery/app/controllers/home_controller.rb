5924df8976dea4f797a15956b13f4688d48763f9
tplamond <tplamond@sfu.ca>
2018-07-19 14:27:05 -0700

    about page

--- pizza_Delivery/app/controllers/home_controller.rb
+++ pizza_Delivery/app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   # before_action :authenticate_user!
 
+  def about
+  end
+
   def index
   end
 end
--- pizza_Delivery/config/routes.rb
+++ pizza_Delivery/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  get 'home/about'
   get 'home/index'
 
   #get 'restaurants/index'
