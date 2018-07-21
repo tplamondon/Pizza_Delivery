ac0cc66053f3f6587325df27ac33b224263df64d
tplamond <tplamond@sfu.ca>
2018-07-21 13:10:00 -0700

    edited controller

--- pizza_Delivery/app/controllers/order_status_controller.rb
+++ pizza_Delivery/app/controllers/order_status_controller.rb
@@ -1,19 +1,25 @@
 class OrderStatusController < ApplicationController
   def index
+    @order_statusus = OrderStatus.all
   end
 
   def inProgress
+    @order_status = OrderStatus.where(id: [1])
   end
 
   def placed
+    @order_status = OrderStatus.where(id: [2])
   end
 
   def shipped
+    @order_status = OrderStatus.where(id: [3])
   end
 
   def completed
+    @order_status = OrderStatus.where(id: [4])
   end
 
   def cancelled
+    @order_status = OrderStatus.where(id: [5])
   end
 end
