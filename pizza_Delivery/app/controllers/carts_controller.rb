1c76f0fb4ffa33f915738063e24c4672227b999b
Duc Lai <ducl@sfu.ca>
2018-07-25 11:50:28 -0700

    Added more to payment page

--- pizza_Delivery/app/controllers/carts_controller.rb
+++ pizza_Delivery/app/controllers/carts_controller.rb
@@ -3,6 +3,8 @@ class CartsController < ApplicationController
     @order_items = current_order.order_items
 	@order = current_order
   end
-  def payment    
+  def payment
+    @order_items = current_order.order_items
+  @order = current_order
   end
 end
--- pizza_Delivery/app/helpers/carts_helper.rb
+++ pizza_Delivery/app/helpers/carts_helper.rb
@@ -1,2 +1,19 @@
 module CartsHelper
+  def ca_provinces
+  [
+    ['Alberta', 'AB'],
+    ['British Columbia', 'BC'],
+    ['Manitoba', 'MB'],
+    ['New Brunswick', 'NB'],
+    ['Newfoundland and Labrador', 'NL'],
+    ['Nova Scotia', 'NS'],
+    ['Northwest Territories', 'NT'],
+    ['Nunavut', 'NU'],
+    ['Ontario', 'ON'],
+    ['Prince Edward Island', 'PE'],
+    ['Quebec', 'QC'],
+    ['Saskatchewan', 'SK'],
+    ['Yukon', 'YT']
+  ]
+end
 end
