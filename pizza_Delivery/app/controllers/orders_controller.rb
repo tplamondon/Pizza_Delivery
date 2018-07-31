64c54b0a26ad6d7e193bef5ba4fa4524faf032d7
tplamond <tplamond@sfu.ca>
2018-07-31 16:36:46 -0700

    update order_status for orders available

--- pizza_Delivery/app/controllers/orders_controller.rb
+++ pizza_Delivery/app/controllers/orders_controller.rb
@@ -13,18 +13,21 @@ class OrdersController < ApplicationController
     redirect_to order_status_index_path
   end
 
+  def edit
+    @order = Order.find(params[:id])
+    @order_status = OrderStatus.all
+  end
+
   def update
     @order = Order.find(params[:id])
-
     if @order.update(order_params)
       redirect_to @order
     else
-      
     end
   end
 
   private
     def order_params
-      params.require(:order).permit(:location)
+      params.require(:order).permit(:location, :order_status_id)
     end
 end
