ccd0c18b3ae2057be85cd8edb735b58121cff587
lplamond <lplamond@sfu.ca>
2018-08-04 13:16:40 -0700

    added name to users

--- pizza_Delivery/app/controllers/users/registrations_controller.rb
+++ pizza_Delivery/app/controllers/users/registrations_controller.rb
@@ -44,12 +44,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :streetAddress, :city, :province, :zipCode])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :streetAddress, :city, :province, :zipCode, :name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :streetAddress, :city, :province, :zipCode])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :streetAddress, :city, :province, :zipCode, :name])
   end
 
   # The path used after sign up.
--- pizza_Delivery/db/schema.rb
+++ pizza_Delivery/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_04_200018) do
+ActiveRecord::Schema.define(version: 2018_08_04_201420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 2018_08_04_200018) do
     t.string "city"
     t.string "province"
     t.string "zipCode"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
