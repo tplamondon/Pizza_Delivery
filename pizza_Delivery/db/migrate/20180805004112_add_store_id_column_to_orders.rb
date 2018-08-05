30bd30bedef1f91a7ef98ebd8b6164e1955146e1
lplamond <lplamond@sfu.ca>
2018-08-04 17:43:37 -0700

    Revert "Added references back to orders through new migration from CLI"
    This reverts commit a1d49ae371c48ce93ef6ac4c3081cf216f9c02d7.

--- pizza_Delivery/db/migrate/20180805004112_add_store_id_column_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddStoreIdColumnToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :orders, :store_id, foreign_key: true
-  end
-end
--- pizza_Delivery/db/migrate/20180805004150_add_driver_id_column_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddDriverIdColumnToOrders < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :orders, :driver_id, foreign_key: true
-  end
-end
